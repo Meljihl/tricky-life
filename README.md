98a9109f9da839a5145a744724f8aaa9274b6143
Malik EL JIHL <malik.eljihl@gmail.com>
2015-11-17 10:46:48 +0100

    Project creation

---
+++ README.md
@@ -0,0 +1,4 @@
+tricky-life
+===========
+
+A Symfony project created on November 17, 2015, 8:49 am.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Product;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $product = new Product();
+        $product->setName('A Foo Bar');
+        $product->setPrice('19.99');
+        $product->setDescription('Lorem ipsum dolor');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($product);
+        $em->flush();
+
+        $repository = $em->getRepository('AppBundle:Product');
+        $numbersList = $repository->findAll();
+
+        return $this->render(
+            'default/index.html.twig',
+            array('luckyNumberList' => $numbersList)
+        );
+
+    }
+}
